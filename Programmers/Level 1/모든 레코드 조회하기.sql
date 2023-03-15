068369f4f747a54f156fb67ec4c9d0199d58d543
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2023-03-15 14:20:43 +0900

    파일정리

--- Programmers/Level 1/모든 레코드 조회하기.sql
+++
@@ -1,4 +0,0 @@
- -- https://school.programmers.co.kr/learn/courses/30/lessons/59034
- -- 모든 동물의 정보를 ANIMAL_IN 순으로 조회하기
-
-SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID ASC;
--- Programmers/Level 1/역순 정렬하기.sql
+++
@@ -1,6 +0,0 @@
--- https://school.programmers.co.kr/learn/courses/30/lessons/59035
--- 동물보호소 동물의 이름과 보호 시작일을 조회
--- 단 Animal_id를 역순으로 출력
-
-SELECT NAME, DATETIME FROM ANIMAL_INS
-ORDER BY ANIMAL_ID DESC;
